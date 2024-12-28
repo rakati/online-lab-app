cb558959ad0b59915856a0deacf7b29a53becaab
rakati <ouhaddounouredine@gmail.com>
2024-12-28 20:42:09 +0100

    Update add get token and refresh token url and remove old one

--- backend/online_lab_app/urls.py
+++ backend/online_lab_app/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from django.views.generic.base import RedirectView
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .users.views import UserViewSet
 
 router = DefaultRouter()
@@ -13,7 +14,10 @@ router.register(r'users', UserViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # Includes browsable API login/logout Interface
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
     # the 'api-root' from django rest-frameworks default router
