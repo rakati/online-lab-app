f921bae4b80b85205e9a662b84c0f28fd4180f75
rakati <ouhaddounouredine@gmail.com>
2024-12-28 21:03:18 +0100

    Update and use env variables

--- backend/wait_for_postgres.py
+++ backend/wait_for_postgres.py
@@ -2,26 +2,30 @@ import os
 import logging
 from time import time, sleep
 import psycopg2
-check_timeout = os.getenv("POSTGRES_CHECK_TIMEOUT", 30)
-check_interval = os.getenv("POSTGRES_CHECK_INTERVAL", 1)
+
+# Get environment variables
+check_timeout = int(os.getenv("POSTGRES_CHECK_TIMEOUT", 30))
+check_interval = int(os.getenv("POSTGRES_CHECK_INTERVAL", 1))
 interval_unit = "second" if check_interval == 1 else "seconds"
+
 config = {
     "dbname": os.getenv("POSTGRES_DB", "postgres"),
     "user": os.getenv("POSTGRES_USER", "postgres"),
     "password": os.getenv("POSTGRES_PASSWORD", ""),
-    "host": os.getenv("DATABASE_URL", "postgres")
+    "host": os.getenv("DATABASE_URL", "postgres"),
 }
 
+# Setup logger
 start_time = time()
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
 
 
-def pg_isready(host, user, password, dbname):
+def pg_isready(dbname, user, password, host):
     while time() - start_time < check_timeout:
         try:
-            conn = psycopg2.connect(**vars())
+            conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host)
             logger.info("Postgres is ready! ✨ 💅")
             conn.close()
             return True
