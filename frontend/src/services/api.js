3c9fd524bca17996186b1dc09730a5f061123dc2
rakati <ouhaddounouredine@gmail.com>
2024-12-28 21:32:40 +0100

    Add login route

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,4 +4,9 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
 
+export const login = async (username, password) => {
+  const response = await api.post('/token/', { username, password });
+  return response.data;
+};
+
 export default api;
