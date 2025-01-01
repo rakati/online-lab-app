5f96580ab2aa2ea64d00653b49d4b12c137aa1d6
rakati <ouhaddounouredine@gmail.com>
2025-01-01 20:35:33 +0100

    include header to the main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './pages/Header';
 import Login from './pages/Login';
 
 function App() {
   return (
     <div>
+      <Header />
       <h1>Online Lab Application</h1>
       <Login />
     </div>
