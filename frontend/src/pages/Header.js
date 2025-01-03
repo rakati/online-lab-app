018eb2f06cc4c04940883cb93240805921e77d3b
rakati <ouhaddounouredine@gmail.com>
2025-01-03 21:28:24 +0100

    update header add link to other pages

--- frontend/src/pages/Header.js
+++ frontend/src/pages/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className="bg-blue-600 text-white p-4">
-      <h1 className="text-xl font-bold">Online Lab Application</h1>
+      <nav>
+        <Link to="/" className="mr-4">Login</Link>
+        <Link to="/profile" className="mr-4">Profile</Link>
+        <Link to="/lab">Lab</Link>
+      </nav>
     </header>
   );
 };
