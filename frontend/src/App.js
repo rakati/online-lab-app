7b7ba5c363bbaaa500e07fa50710fc6fd0ca9cb9
rakati <ouhaddounouredine@gmail.com>
2025-01-03 21:26:58 +0100

    Add route to new pages Lab, Profile, root

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Header from './pages/Header';
 import Login from './pages/Login';
+import Profile from './pages/Profile';
+import Lab from './pages/Lab';
 
 function App() {
   return (
     <div>
       <Header />
       <h1>Online Lab Application</h1>
-      <Login />
+
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/lab" element={<Lab />} />
+      </Routes>
     </div>
   );
 }
