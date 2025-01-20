f4086b1f8483f61db4a9e6f085a2ad3c5d626843
rakati <ouhaddounouredine@gmail.com>
2025-01-20 16:59:51 +0100

    set a color for sidebar

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-  darkMode: 'class', // Enable class-based dark mode
+  darkMode: 'class', // Enable dark mode
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sidebar: '#1F2937', // Example custom color for sidebar
+      },
+    },
   },
   plugins: [],
 };
