68327b91741a6a3e0b981ed0fbe80074f9037891
rakati <ouhaddounouredine@gmail.com>
2025-01-22 17:47:32 +0100

    wrap sensible route with route protector requiring login before use

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import LabPage from './pages/LabPage';
@@ -7,6 +7,7 @@ import RegisterPage from './pages/RegisterPage';
 import ProfilePage from './pages/ProfilePage';
 import LandingPage from './pages/LandingPage';
 import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   const [theme, setTheme] = useState(localStorage.getItem('theme') || 'dark');
@@ -28,10 +29,23 @@ function App() {
             <div className="flex-1 overflow-y-auto bg-gray-900 text-gray-300">
               <Routes>
                 <Route path="/" element={<LandingPage />} />
-                <Route path="/lab" element={<LabPage />} />
+                <Route
+                  path="/lab"
+                  element={
+                    <ProtectedRoute>
+                      <LabPage />
+                    </ProtectedRoute>
+                  }
+                />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
-                <Route path="/profile" element={<ProfilePage />} />
+                <Route
+                  path="/profile"
+                  element={
+                    <ProtectedRoute>
+                      <ProfilePage />
+                    </ProtectedRoute>}
+                />
               </Routes>
             </div>
           </div>
