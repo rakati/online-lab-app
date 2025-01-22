bef68884bbf8c070456a14b10a90401ed9d0fe1d
rakati <ouhaddounouredine@gmail.com>
2025-01-22 17:05:58 +0100

    use strict mode to catch bugs earlier

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter as Router} from 'react-router-dom';
+import { StrictMode } from 'react';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
-    <App />
+    <StrictMode>
+      <App />
+
+    </StrictMode>
   </Router>
 );
 
