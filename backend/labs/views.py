2e9306a4fe4992847817d3e76708d8c28b8417d5
rakati <ouhaddounouredine@gmail.com>
2025-01-25 21:12:43 +0100

    Create Lab and studentprogress views

--- backend/labs/views.py
+++ backend/labs/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from online_lab_app.users.permissions import IsInstructor, IsStudent
+from .models import Lab, StudentProgress
+from .serializers import LabSerializer, StudentProgressSerializer
 
-# Create your views here.
+
+class LabViewSet(ModelViewSet):
+    queryset = Lab.objects.all()
+    serializer_class = LabSerializer
+    permission_classes = [IsAuthenticated, IsInstructor]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
+
+class StudentProgressViewSet(ModelViewSet):
+    queryset = StudentProgress.objects.all()
+    serializer_class = StudentProgressSerializer
+    permission_classes = [IsAuthenticated, IsStudent]
