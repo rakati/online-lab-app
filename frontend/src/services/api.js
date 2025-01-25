95da82c979dcc8610efacb85aaa4e75231d78d5c
rakati <ouhaddounouredine@gmail.com>
2025-01-25 21:23:48 +0100

    intercept request and check header authorization for access token

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,6 +4,15 @@ const api = axios.create({
   baseURL: 'http://localhost:8000/api',
 });
 
+// Add Authorization header to all requests
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('access'); // Retrieve the access token
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export const login = async (username, password) => {
   const response = await api.post('/token/', { username, password });
   return response.data;
