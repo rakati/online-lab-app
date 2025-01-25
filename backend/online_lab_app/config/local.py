ea3b7c8c571389a918aa1fe7c7860a5638f3a713
rakati <ouhaddounouredine@gmail.com>
2025-01-25 21:20:27 +0100

    allow all origine from loca host with any port to be used by drf UI

--- backend/online_lab_app/config/local.py
+++ backend/online_lab_app/config/local.py
@@ -1,5 +1,6 @@
 import os
 from .common import Common
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -9,7 +10,16 @@ class Local(Common):
     # Testing
     INSTALLED_APPS = Common.INSTALLED_APPS
 
+    # start ------ allowing drf to recognize jwt token
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        r"^https?://localhost(:[0-9]+)?$",  # Allow localhost with any port
+        r"^https?://127\.0\.0\.1(:[0-9]+)?$",  # Allow 127.0.0.1 with any port
+    ]
+    CORS_EXPOSE_HEADERS = [
+        "Authorization",
+    ]
+    # ------------ end drf
     # Mail
-    EMAIL_HOST = 'localhost'
+    EMAIL_HOST = "localhost"
     EMAIL_PORT = 1025
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
