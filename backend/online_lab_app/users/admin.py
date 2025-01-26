0908cea9ad6dfce0133f9accdc6f92988078f5a8
rakati <ouhaddounouredine@gmail.com>
2025-01-26 22:58:48 +0100

    update user admin to handle new input field for user added info

--- backend/online_lab_app/users/admin.py
+++ backend/online_lab_app/users/admin.py
@@ -1,8 +1,45 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
 
 @admin.register(User)
-class UserAdmin(UserAdmin):
-    pass
+class UserAdmin(BaseUserAdmin):
+    # Displayed fields in the user list
+    list_display = ("username", "email", "role", "birthday", "is_active", "is_staff")
+    list_filter = ("role", "is_active", "is_staff")
+    search_fields = ("username", "email", "first_name", "last_name")
+
+    # Fields in the user form
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (
+            "Personal Info",
+            {"fields": ("first_name", "last_name", "email", "birthday", "avatar")},
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Role", {"fields": ("role",)}),
+        ("Important Dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2", "role", "email"),
+            },
+        ),
+    )
+
+    ordering = ("username",)
