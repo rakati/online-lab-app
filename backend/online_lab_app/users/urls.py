bde7e995811ac6bb104d3b6b146a34e06b428592
rakati <ouhaddounouredine@gmail.com>
2025-01-26 23:17:00 +0100

    add url for getting profile and changing password

--- backend/online_lab_app/users/urls.py
+++ backend/online_lab_app/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import AssignRoleView, RegisterView
+from .views import AssignRoleView, RegisterView, ProfileView, ChangePasswordView
 
 urlpatterns = [
     path("assign-role/", AssignRoleView.as_view(), name="assign_role"),
     path("register/", RegisterView.as_view(), name="register"),
+    path("profile/", ProfileView.as_view(), name="profile"),
+    path("change-password/", ChangePasswordView.as_view(), name="change_password"),
 ]
