6659c8c3d69e45ff70f4231f195b7b7e180b1f3a
rakati <ouhaddounouredine@gmail.com>
2025-01-27 01:12:59 +0100

    modify profile serializer to accept patch update

--- backend/online_lab_app/users/serializers.py
+++ backend/online_lab_app/users/serializers.py
@@ -62,3 +62,9 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = User
         fields = ["username", "email", "first_name", "last_name", "birthday", "avatar"]
         read_only_fields = ["username", "email"]
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
