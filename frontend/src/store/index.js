774687c4bf64690ddf99510e2e57b9ed09ea60f0
rakati <ouhaddounouredine@gmail.com>
2025-01-28 21:44:43 +0100

    install and use redux for global state of the theme

---
+++ frontend/src/store/index.js
@@ -0,0 +1,13 @@
+// src/store/index.js
+import { configureStore } from '@reduxjs/toolkit';
+import themeReducer from './themeSlice';
+// import userReducer from './userSlice';
+// import labsReducer from './labsSlice';
+
+export const store = configureStore({
+  reducer: {
+    theme: themeReducer,
+    // user: userReducer,
+    // labs: labsReducer,
+  },
+});
