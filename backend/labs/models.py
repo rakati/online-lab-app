1669fcc36b76f46dbcd7cc546fa1977db7b29c2e
rakati <ouhaddounouredine@gmail.com>
2025-01-30 21:29:29 +0100

    add participants filed to lab table

--- backend/labs/models.py
+++ backend/labs/models.py
@@ -13,19 +13,25 @@ class Lab(models.Model):
     )
 
     title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    image = models.ImageField(upload_to="labs/", blank=True, null=True)
+
     description = models.TextField(blank=True, default="")
     instructions = models.TextField(blank=True, default="")
-    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(
         max_length=20,
         choices=STATUS_CHOICES,
         default="DRAFT",
         help_text="Indicates if the lab is published or still in draft.",
     )
-    image = models.ImageField(upload_to="labs/", blank=True, null=True)
-    slug = models.SlugField(max_length=255, unique=True, blank=True)
+
     packages = JSONField(default=list, blank=True)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    participants = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="joined_labs", blank=True
+    )
 
     def __str__(self):
         return self.title
