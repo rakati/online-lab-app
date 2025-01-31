8692c7ce1a849586239bccd16e806a08ed9d0f5f
rakati <ouhaddounouredine@gmail.com>
2025-01-31 19:53:03 +0100

    Fix: add missing import,, fix request typo, and role checking

--- backend/labs/views.py
+++ backend/labs/views.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from django.db.models import Q
 from online_lab_app.users.permissions import IsInstructor, IsStudent
 from .models import Lab, StudentProgress
 from .serializers import LabSerializer, StudentProgressSerializer
@@ -16,15 +17,15 @@ class LabViewSet(ModelViewSet):
         serializer.save(created_by=self.request.user)
 
     def get_queryset(self):
-        user = self.requrest.user
+        user = self.request.user
 
-        if user.is_instructor:
+        if user.role == "instructor":
             return Lab.objects.filter(
-                models.Q(created_by=user) | models.Q(status="PUBLISHED")
+                Q(created_by=user) | Q(status="PUBLISHED")
             ).distinct()
         else:
             return Lab.objects.filter(
-                models.Q(status="PUBLISHED") | models.Q(participants=user)
+                Q(status="PUBLISHED") | Q(participants=user)
             ).distinct()
 
     @action(detail=True, methods=["post"], url_path="subscribe")
