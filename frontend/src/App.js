c70688636deeb0461ab9d9eb4d04e2e931640145
rakati <ouhaddounouredine@gmail.com>
2025-02-01 23:15:45 +0100

    add new route for preview tutorial

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import ProfilePage from './pages/ProfilePage';
 import LandingPage from './pages/LandingPage';
 import CreateLabPage from './pages/CreateLabPage';
 import LabsPage from './pages/LabsPage';
+import LabInstructionPage from './pages/LabInstructionsPage';
 import DashboardPage from './pages/DashboardPage';
 import { ProtectedRoute, PublicOnlyRoute } from './components/ProtectedRoute';
 
@@ -54,7 +55,8 @@ function App() {
               <Route path="/labs"           element={<ProtectedRoute>  <LabsPage />         </ProtectedRoute>}/>
               <Route path="/labs/:id"       element={<ProtectedRoute>  <LabInfoPage />      </ProtectedRoute>}/>
               <Route path="/labs/:id/edit"  element={<ProtectedRoute>  <LabInfoPage />      </ProtectedRoute>}/>
-              <Route path="/labs/:id/start"  element={<ProtectedRoute>  <LabPage />          </ProtectedRoute>}/>
+              <Route path="/labs/:id/start"  element={<ProtectedRoute>  <LabInfoPage />      </ProtectedRoute>}/>
+              <Route path="/labs/:id/view"  element={<ProtectedRoute> <LabInstructionPage/></ProtectedRoute>}/>
               <Route path="/labs/new"        element={<ProtectedRoute>  <CreateLabPage />    </ProtectedRoute>}/>
               {/* temp */}
               <Route path="/lab"            element={<ProtectedRoute>  <LabPage />          </ProtectedRoute>}/>
