3a10a9a9b336f70f41bfda0e124bf15d514f713f
rakati <ouhaddounouredine@gmail.com>
2025-02-04 18:28:54 +0100

    handle logic for get lab list and handle lab start using theia logic

--- backend/labs/views.py
+++ backend/labs/views.py
@@ -2,10 +2,11 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from .theia_utils import run_theia_container
 from django.db.models import Q
 from online_lab_app.users.permissions import IsInstructor, IsStudent
 from .models import Lab, StudentProgress
-from .serializers import LabSerializer, StudentProgressSerializer
+from .serializers import LabSerializer, StudentProgressSerializer, LabListSerializer
 
 
 class LabViewSet(ModelViewSet):
@@ -13,13 +14,18 @@ class LabViewSet(ModelViewSet):
     serializer_class = LabSerializer
     permission_classes = [IsAuthenticated]
 
+    def get_serializer_class(self):
+        if self.action == "list":
+            return LabListSerializer
+        return LabSerializer
+
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
 
     def get_queryset(self):
         user = self.request.user
 
-        if user.role == "instructor":
+        if user.role == "instructor" or "admin":
             return Lab.objects.filter(
                 Q(created_by=user) | Q(status="PUBLISHED")
             ).distinct()
@@ -42,6 +48,33 @@ class LabViewSet(ModelViewSet):
         user = request.user
         lab.participants.remove(user)
         return Response({"status": "unsubscribed"})
+
+    @action(detail=True, methods=["post"], url_path="start-lab")
+    def start_lab(self, request, pk=None):
+        """
+        custom endpoint triggers the container for Theia.
+        """
+        lab = self.get_object()
+        user = request.user
+
+        if not lab.participants.filter(id=user.id).exists():
+            lab.participants.add(user)
+
+        # Construct container_name, port, workspace_dir
+        container_name = f"theia_{user.id}_{lab.id}"
+        port = 4000  # or dynamic
+        workspace_dir = f"user_{user.id}_lab_{lab.id}"
+
+        # packages = lab.packages  # might be a list from the DB
+        container = run_theia_container(container_name, port, workspace_dir)
+        return Response(
+            {
+                "status": "running",
+                "containerName": container_name,
+                "port": port,
+                "url": f"http://{request.get_host()}:{port}",
+            }
+        )
 
 
 class StudentProgressViewSet(ModelViewSet):
