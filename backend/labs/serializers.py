0b3f090f87df85720881b8a0679e47fbab321e8f
rakati <ouhaddounouredine@gmail.com>
2025-02-04 18:25:11 +0100

    adapt new fields in the serializer and create custom serializer for lab list

--- backend/labs/serializers.py
+++ backend/labs/serializers.py
@@ -3,6 +3,11 @@ from .models import Lab, StudentProgress
 
 
 class LabSerializer(serializers.ModelSerializer):
+    creator_username = serializers.ReadOnlyField(
+        source="created_by.username", read_only=True
+    )
+    is_participant = serializers.SerializerMethodField()
+
     class Meta:
         model = Lab
         fields = (
@@ -15,10 +20,46 @@ class LabSerializer(serializers.ModelSerializer):
             "status",
             "packages",
             "participants",
+            "time",
+            "skills",
+            "difficulty",
+            "language",
+            "tags",
+            "creator_username",
+            "is_participant",
             "created_by",
             "created_at",
         )
         read_only_fields = ("created_by", "created_at", "participants", "slug")
+
+    def get_is_participant(self, obj):
+        request = self.context.get("request")
+        if request and request.user.is_authenticated:
+            return obj.participants.filter(id=request.user.id).exists()
+        return False
+
+
+class LabListSerializer(serializers.ModelSerializer):
+    description_snippet = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Lab
+        fields = (
+            "id",
+            "title",
+            "image",
+            "description_snippet",
+            "time",
+            "skills",
+            "difficulty",
+            "language",
+            "tags",
+        )
+
+    def get_description_snippet(self, obj):
+        if len(obj.description) > 100:
+            return obj.description[:100] + "..."
+        return obj.description
 
 
 class StudentProgressSerializer(serializers.ModelSerializer):
